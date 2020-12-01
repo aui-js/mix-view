555715b91d023ab20f2e8490425377691a272612
[aui-js] <[cl15095344637@163.com]>
2020-12-01 10:05:09 +0800

    loading组件修改

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -1,3 +1,39 @@
-import Loading from './src/loading.vue'
+import Loading from './src/index.vue'
 
-export default Loading
+let $loading
+
+export default {
+   install(Vue, options) {
+      if (!$vm) {
+         const LoadingPlugin = Vue.extend(Loading);
+
+         $vm = new LoadingPlugin({
+            el: document.createElement('div')
+         });
+
+         document.body.appendChild($vm.$el);
+      }
+
+      $vm.show = false;
+
+      let loading = {
+         show(text) {
+            $vm.show = true;
+            $vm.text = text;
+         },
+         hide() {
+            $vm.show = false;
+         }
+      };
+
+      if (!Vue.$loading) {
+         Vue.$loading = loading;
+      }
+
+      Vue.mixin({
+         created() {
+            this.$loading = Vue.$loading;
+         }
+      })
+   }
+}
--- src/components/loading/src/index.vue
+++ src/components/loading/src/index.vue
@@ -1,209 +1,23 @@
+<!-- loading.vue组件 -->
 <template>
-   <div>
-      <div class="loader kd-column">
-         <div class="loader-inner">
-            <div class="loader-line-wrap">
-               <div class="loader-line"></div>
-            </div>
-            <div class="loader-line-wrap">
-               <div class="loader-line"></div>
-            </div>
-            <div class="loader-line-wrap">
-               <div class="loader-line"></div>
-            </div>
-            <div class="loader-line-wrap">
-               <div class="loader-line"></div>
-            </div>
-            <div class="loader-line-wrap">
-               <div class="loader-line"></div>
-            </div>
+   <div class="loading-box" v-show="show">
+      <div class="loading-mask"></div>
+      <div class="loading-content">
+         <div class="animate">
          </div>
-         <div class="kd-row">
-            <span class="loading-text">{{tip}}</span>
-         </div>
+         <div class="text">{{text}}</div>
       </div>
-      <my-icon style="display: inline-block;font-size:58px" :class="classes" type="small-loading"></my-icon>
    </div>
 </template>
 
 <script>
-   import myIcon from '../../icon'
-   const prefixCls = 'myzx-btn'
    export default {
-      name: 'my-loading',
       props: {
-         tip: {
+         show: Boolean,
+         text: {
             type: String,
-            default: '更新中'
-         }
-      },
-      components: {
-         myIcon
-      },
-      data() {
-         return {}
-      },
-      created() {},
-      async mounted() {
-         console.log(this)
-      },
-      watch: {},
-      computed: {
-         classes: () => {
-            return [
-               `${prefixCls}-loading-roate`,
-               {}
-            ]
-         }
-      },
-      updated() {
-         // 给父类的window传递当前iframe的高度
-      },
-      methods: {}
+            default: '正在加载中...'
+         },
+      }
    }
 </script>
-
-<style lang="less" scoped>
-   .loader {
-      z-index: 99999;
-   }
-
-   .loader-inner {
-      width: 100px;
-      position: relative;
-   }
-
-   .kd-row {
-      display: flex;
-      flex-direction: row;
-      flex-wrap: nowrap;
-      justify-content: center;
-   }
-
-   .kd-column {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-   }
-
-   .loading-text {
-      display: block;
-      margin-top: 120px;
-      color: #2d8cf0;
-   }
-
-   .loader-line-wrap {
-      -webkit-animation: spin 2000ms cubic-bezier(.175, .885, .32, 1.275) infinite;
-      animation: spin 2000ms cubic-bezier(.175, .885, .32, 1.275) infinite;
-      box-sizing: border-box;
-      height: 50px;
-      left: 0;
-      overflow: hidden;
-      position: absolute;
-      top: 0;
-      -webkit-transform-origin: 50% 100%;
-      transform-origin: 50% 100%;
-      width: 100px;
-   }
-
-   .loader-line {
-      border: 4px solid transparent;
-      border-radius: 100%;
-      box-sizing: border-box;
-      height: 100px;
-      left: 0;
-      margin: 0 auto;
-      position: absolute;
-      right: 0;
-      top: 0;
-      width: 100px;
-   }
-
-   .loader-line-wrap:nth-child(1) {
-      -webkit-animation-delay: -50ms;
-      animation-delay: -50ms;
-   }
-
-   .loader-line-wrap:nth-child(2) {
-      -webkit-animation-delay: -100ms;
-      animation-delay: -100ms;
-   }
-
-   .loader-line-wrap:nth-child(3) {
-      -webkit-animation-delay: -150ms;
-      animation-delay: -150ms;
-   }
-
-   .loader-line-wrap:nth-child(4) {
-      -webkit-animation-delay: -200ms;
-      animation-delay: -200ms;
-   }
-
-   .loader-line-wrap:nth-child(5) {
-      -webkit-animation-delay: -250ms;
-      animation-delay: -250ms;
-   }
-
-   .loader-line-wrap:nth-child(1) .loader-line {
-      border-color: hsl(0, 80%, 60%);
-      height: 90px;
-      width: 90px;
-      top: 7px;
-   }
-
-   .loader-line-wrap:nth-child(2) .loader-line {
-      border-color: hsl(60, 80%, 60%);
-      height: 76px;
-      width: 76px;
-      top: 14px;
-   }
-
-   .loader-line-wrap:nth-child(3) .loader-line {
-      border-color: hsl(120, 80%, 60%);
-      height: 62px;
-      width: 62px;
-      top: 21px;
-   }
-
-   .loader-line-wrap:nth-child(4) .loader-line {
-      border-color: hsl(180, 80%, 60%);
-      height: 48px;
-      width: 48px;
-      top: 28px;
-   }
-
-   .loader-line-wrap:nth-child(5) .loader-line {
-      border-color: hsl(240, 80%, 60%);
-      height: 34px;
-      width: 34px;
-      top: 35px;
-   }
-
-   @-webkit-keyframes spin {
-
-      0%,
-      15% {
-         -webkit-transform: rotate(0);
-         transform: rotate(0);
-      }
-
-      100% {
-         -webkit-transform: rotate(360deg);
-         transform: rotate(360deg);
-      }
-   }
-
-   @keyframes spin {
-
-      0%,
-      15% {
-         -webkit-transform: rotate(0);
-         transform: rotate(0);
-      }
-
-      100% {
-         -webkit-transform: rotate(360deg);
-         transform: rotate(360deg);
-      }
-   }
-</style>
